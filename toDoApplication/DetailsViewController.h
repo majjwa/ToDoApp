0df8b9222a00d45187ce9bde798a3f928d32aaae
Marwa Maky <marwamaky50@gmail.com>
2024-08-12 03:15:02 +0300

    Updating app's ui
    updating the details screen

---
+++ toDoApplication/DetailsViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailsViewController.h
+//  toDoApplication
+//
+//  Created by marwa maky on 12/08/2024.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- toDoApplication/toDoApplication/TodoNotesViewController.m
+++ toDoApplication/toDoApplication/TodoNotesViewController.m
@@ -18,14 +18,11 @@
     // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)addEditButton:(UIBarButtonItem *)sender {
+    UIViewController *detailsvc = [self.storyboard instantiateViewControllerWithIdentifier:@"DetailsViewController"];
+    [self.navigationController pushViewController:detailsvc animated:YES];
+    
 }
-*/
+
 
 @end
